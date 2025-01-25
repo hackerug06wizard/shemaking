484e9f8895887904df0bb07bf90f317982267e9d
hackerug06wizard <hackerugg06@gmail.com>
2025-01-26 00:35:18 +0300

    Update page.tsx

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,5 +1,5 @@
-import { AnimatedHeading } from "@/components/animated-heading"
-import { ArtistProfile } from "@/components/artist-profile"
+import { AnimatedHeading } from "@./components/animated-heading"
+import { ArtistProfile } from "@./components/artist-profile"
 
 export default function About() {
   return (
