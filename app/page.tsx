faa0b70ea7ec9bd31775aba9e8ae04e54379ef6d
hackerug06wizard <hackerugg06@gmail.com>
2025-01-25 10:29:38 +0300

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
-import { ArtistProfile } from "./components/ui/artist-profile"
-import { SongList } from "./components/ui/song-list.tsx"
-import { UpcomingEvents } from "./components/ui/upcoming-events.tsx"
-import { AnimatedHeading } from "./components/ui/animated-heading.tsx"
+import { ArtistProfile } from "@/components/artist-profile"
+import { SongList } from "@/components/song-list"
+import { UpcomingEvents } from "@/components/upcoming-events"
+import { AnimatedHeading } from "@/components/animated-heading"
 
 export default function Home() {
   return (
@@ -14,4 +14,3 @@ export default function Home() {
   )
 }
 
-    
