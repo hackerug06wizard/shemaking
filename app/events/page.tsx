94209d1f92584dca22ab0da2a42b7f198d8aace8
hackerug06wizard <hackerugg06@gmail.com>
2025-01-25 10:48:12 +0300

    Update page.tsx

--- app/events/page.tsx
+++ app/events/page.tsx
@@ -1,5 +1,5 @@
-import { AnimatedHeading } from "@/components/animated-heading"
-import { UpcomingEvents } from "@/components/upcoming-events"
+import { AnimatedHeading } from "./components/animated-heading.tsx"
+import { UpcomingEvents } from "./components/upcoming-events.tsx"
 
 export default function Events() {
   return (
