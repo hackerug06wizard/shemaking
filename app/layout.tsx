0659cb5a146059152942bd06f0da9c50f954789e
hackerug06wizard <hackerugg06@gmail.com>
2025-01-26 01:26:18 +0300

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
-import "@/styles/globals.css"
+import "./styles/globals.css"
 import { Inter } from "next/font/google"
-import { NavBar } from "@/components/nav-bar"
+import { NavBar } from "./components/nav-bar"
 
 const inter = Inter({ subsets: ["latin"] })
 
